e857da18d8203beb370f6ef3bd882e64f08a8746
JackDarius <clement.flance@gmail.com>
2016-10-25 19:47:18 +0200

    Update the furball position - no physic

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,21 +15,14 @@ public class Movement : MonoBehaviour
 	
     void Update()
     {
-        if(transform.localScale.x > _scale / 0.5 && _isMoving)
+        if (transform.localScale.x > _scale / 0.5 && _isMoving)
+        {
             transform.localScale = new Vector3(transform.localScale.x - _speed * _scale / 3 * 7f * Time.deltaTime, transform.localScale.y - _speed * _scale / 3 * 7f * Time.deltaTime, transform.localScale.z);
-    }
-
-	void FixedUpdate ()
-    {
-        transform.GetComponent<Rigidbody2D>().AddForce(_targetDirection * _speed);
+            transform.position += (Vector3)_targetDirection * _speed * Time.deltaTime;
+        }
 
         if (transform.localScale.x <= _scale / 0.5)
-        {
-            transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            transform.GetComponent<Rigidbody2D>().angularVelocity = 0f;
-            transform.GetComponent<Rigidbody2D>().isKinematic = true;
             _isMoving = false;
-        }
     }
     
     //Setter
